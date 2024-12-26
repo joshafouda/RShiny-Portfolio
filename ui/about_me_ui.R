2e10d6af1406f41d8c74c601504e084f8d3d27a4
joshafouda <josh.aremou229@gmail.com>
2024-12-26 05:29:22 +0100

    background in logo colors for "about me" menu

--- ui/about_me_ui.R
+++ ui/about_me_ui.R
@@ -16,16 +16,16 @@ liste_tabitems <- append(liste_tabitems, list(
       
       # Full-screen background section
       div(
-        style = "background-image: url('images/background.jpg'); background-size: cover; background-position: center; height: 100vh; display: flex; align-items: center; justify-content: center;",
+        style = "background: linear-gradient(135deg, #FF8C00, #32CD32); height: 100vh; display: flex; align-items: center; justify-content: center; color: white; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.8);",
         div(
-          style = "background-color: rgba(255, 255, 255, 0.8); padding: 2rem; border-radius: 10px; display: flex; flex-direction: row; align-items: center;",
+          style = "background-color: rgba(255, 255, 255, 0.9); padding: 2rem; border-radius: 10px; display: flex; flex-direction: row; align-items: center; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);",
           tags$img(
             src = "images/linkedin_profil.png", 
-            style = "width: 150px; height: 150px; border-radius: 50%; margin-right: 2rem;"
+            style = "width: 150px; height: 150px; border-radius: 50%; margin-right: 2rem; border: 3px solid #FF8C00;"
           ),
           div(
             style = "max-width: 600px; text-align: left;",
-            h2("Josué (Joshua) AFOUDA", style = "font-weight: bold; color: #2C3E50;"),
+            h2("Josué AFOUDA", style = "font-weight: bold; color: #2C3E50;"),
             p("Experienced Data Scientist with over 6 years of expertise in building data-driven solutions and innovative applications.", style = "font-size: 18px; line-height: 1.5; color: #2C3E50;"),
             p("Passionate about data visualization, machine learning, and simplifying complex data processes for impactful results.", style = "font-size: 18px; line-height: 1.5; color: #2C3E50;"),
             p("Connect with me on LinkedIn or subscribe to my YouTube channel for insightful content on data science.", style = "font-size: 18px; line-height: 1.5; color: #2C3E50;"),
